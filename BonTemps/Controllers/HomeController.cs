7496cfe692c972b8dcfe48c96aa7a7f68073064c
Kai <kaischool122@gmail.com>
2018-01-19 14:37:46 +0100

    Datasource voor charts index file & seed method

--- BonTemps/Controllers/HomeController.cs
+++ BonTemps/Controllers/HomeController.cs
@@ -18,7 +18,42 @@ namespace BonTemps.Controllers
             string currentUserId = User.Identity.GetUserId();
             ApplicationUser currentUser = db.Users.FirstOrDefault(x => x.Id == currentUserId);
             if (currentUser != null) ViewBag.username = currentUser.UserName;
-            return View();
+
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            int months = 5;
+
+            int[] bestellingen = new int[months];
+
+            for (var i = 0; i < months; i++)
+            {
+                var today = DateTime.Today;
+                var month = new DateTime(today.Year, today.Month, 1);
+                DateTime first;
+                DateTime last;
+                string keymonth;
+
+                if (i != 0)
+                {
+                    first = month.AddMonths(-i);
+                    last = month.AddMonths(-(i - 1)).AddDays(-1);
+                    keymonth = month.AddMonths(-(i - 1)).AddDays(-1).ToString("MMM");
+                }
+                else
+                {
+                    first = month.AddMonths(0);
+                    last = month.AddMonths(1).AddDays(-1);
+                    keymonth = month.AddMonths(1).AddDays(-1).ToString("MMM");
+                }
+                int count = db.Customers.Count(m => m.DateCreated >= first && m.DateCreated <= last);
+                bestellingen[i] = db.Reservations.Count(m => m.DateCreated >= first && m.DateCreated <= last);
+                dictionary.Add(keymonth, count);
+            }
+            ViewBag.lastMonthsData = dictionary.Reverse();
+            ViewBag.lastMonthsReservations = bestellingen.Reverse();
+
+
+                return View();
         }
     }
 }
--- BonTemps/Migrations/Configuration.cs
+++ BonTemps/Migrations/Configuration.cs
@@ -16,15 +16,31 @@ namespace BonTemps.Migrations
 
         protected override void Seed(BonTemps.Models.ApplicationDbContext context)
         {
-            for (var i = 1; i < 10; i++)//loop 9 times
+            if(!context.Table_layout.Any())
             {
-                for (var j = 1; j < 10; j++)//loop 9 times
+                //Seed table layout
+                for (var i = 1; i < 10; i++)//loop 9 times
                 {
-                    context.Table_layout.AddOrUpdate(
-                        new Table_layout { LayoutX = i, LayoutY = j, IsTable = true}
+                    for (var j = 1; j < 10; j++)//loop 9 times
+                    {
+                        context.Table_layout.AddOrUpdate(
+                            new Table_layout { LayoutX = i, LayoutY = j, IsTable = true }
+                        );
+                    }
+                }
+            }
+
+            if (!context.Customers.Any())
+            {
+                //seed customers table
+                for (var i = 0; i < 500; i++)
+                {
+                    context.Customers.AddOrUpdate(
+                        new Customers { Gender = GenderEnum.man, FirstName = "Customer" + i, Prefix = "", LastName = "Seed" + i, PhoneNumber = 061223123 + i, Email = "Customer" + i + "@localhost", NewsLetter = true, DateCreated = DateTime.Now.AddDays(-new Random().Next(0, 190)) }
                     );
                 }
             }
+            
 
         }
     }
