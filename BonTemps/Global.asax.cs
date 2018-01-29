8275d0c0706cd87bf6954661108827f5c6432e07
Jelle Wevers <jellewevers@outlook.com>
2018-01-29 20:59:55 +0100

    Role manager added

--- BonTemps/Global.asax.cs
+++ BonTemps/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BonTemps.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +13,8 @@ namespace BonTemps
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         protected void Application_Start()
         {
             //System.Globalization.CultureInfo.DefaultThreadCurrentCulture = new System.Globalization.CultureInfo("nl-NL");
@@ -19,6 +23,21 @@ namespace BonTemps
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Session_Start(Object sender, EventArgs e)
+        {
+
+            var store = new UserStore<ApplicationUser>(db);
+            var manager = new ApplicationUserManager(store);
+            var username = System.Threading.Thread.CurrentPrincipal.Identity.Name;
+
+            if (!string.IsNullOrEmpty(username))
+            {
+                var user = db.Users.FirstOrDefault(u => u.UserName == username);
+                var roles = manager.GetRolesAsync(user.Id).Result;
+                Session["roles"] = roles;
+            }
         }
     }
 }
---
+++ BonTemps/Models/RolesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BonTemps.Models
+{
+    public class RolesViewModel
+    {
+        public IEnumerable<string> RoleNames { get; set; }
+        public string UserName { get; set; }
+    }
+}
